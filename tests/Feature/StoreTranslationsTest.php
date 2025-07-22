e0214d3b25732a38f162e56141cf74e22ca16f38
jonaaix <jonaaix@icloud.com>
2025-07-22 14:33:41 +0200

    Test mass assignment

--- tests/Feature/StoreTranslationsTest.php
+++ tests/Feature/StoreTranslationsTest.php
@@ -49,3 +49,39 @@ it('stores multiple translations instantly via storeTranslations()', function ()
    assertDatabaseHas('test_model_translations', ['locale' => 'de', 'translation' => 'German Name']);
    assertDatabaseHas('test_model_translations', ['locale' => 'fr', 'translation' => 'French Name']);
 });
+
+it('stages translations via multi-locale array assignment (Spatie-style)', function () {
+    $model = new TestModel();
+
+    $model->name = [
+        'de' => 'German Name',
+        'fr' => 'French Name',
+    ];
+
+    $model->save();
+
+    assertDatabaseHas('test_model_translations', ['locale' => 'de', 'translation' => 'German Name']);
+    assertDatabaseHas('test_model_translations', ['locale' => 'fr', 'translation' => 'French Name']);
+});
+
+it('prioritizes multi-locale array assignment over active translation mode', function () {
+    $model = new TestModel();
+
+    // Put the model into a persistent locale mode.
+    $model->setLocale('es');
+
+    // Assign a multi-locale array. This should take precedence over the Spanish locale mode.
+    $model->name = [
+        'de' => 'German Name',
+        'fr' => 'French Name',
+    ];
+
+    $model->save();
+
+    // Assert that the array's locales were saved.
+    assertDatabaseHas('test_model_translations', ['locale' => 'de', 'translation' => 'German Name']);
+    assertDatabaseHas('test_model_translations', ['locale' => 'fr', 'translation' => 'French Name']);
+
+    // Assert that the persistent locale was ignored for this assignment.
+    $this->assertDatabaseMissing('test_model_translations', ['locale' => 'es']);
+});
