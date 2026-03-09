0ba05363e9054737ca7b9de96787d2b9eddd5bc9
bilalelhaj <bilal.devco@gmail.com>
2026-03-09 02:48:37 +0100

    Fix TypeError in resolveTranslatedValue for JSON casted attributes

--- src/Traits/HasTranslations.php
+++ src/Traits/HasTranslations.php
@@ -123,7 +123,7 @@ trait HasTranslations
          : Str::snake(class_basename($this)) . '_' . $this->getKeyName();
    }
 
-   protected function resolveTranslatedValue(string $column, ?string $locale): ?string
+   protected function resolveTranslatedValue(string $column, ?string $locale): mixed
    {
       $localesToCheck = array_unique(
          array_filter([$locale, $this->getActiveLocale(), App::getLocale(), Config::get('translatable.fallback_locale')]),
